d44d3c30cc361b49050ee528885c8bed422830f4
kikuta <kazumasa.ikuta@gmail.com>
2019-08-19 11:22:48 +0900

    Onbox

---
+++ Python/hello.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+url = "http://10.71.154.116:3000/contractlist"
+
+headers = {"content-type": "application/json"}
+r = requests.get(url, headers=headers)
+
+data = r.json()
+print json.dumps(data, indent=4)
